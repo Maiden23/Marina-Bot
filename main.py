08332d69b3a91856e8d90b1347a1486dbc577f6e
Angel Maria Stanley <88926570+Maiden23@users.noreply.github.com>
2021-10-18 04:20:20 +0000

    added few new characters

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ client = discord.Client()
 
 
 characters = ["Glitch","Zawardo","timeleaper","morph"]
-glitch = "Thank you for choosing me , I have psychi powers and i hope i can help you:)"
-
+glitch = "Arigato. I have psychi powers and i hope i can help you:)"
+zawardo = "Gracias .I Have the power to manipulate all 5 elements. Let's give it all. "
+timeleaper = "Merci .I can mainpulate time. Let's change time together . " 
+morph = "Grazie . I have the power to change from one form to another. "
 @client.event
 async def on_ready():
   print('Hello {0.user}'.format (client))
@@ -24,14 +26,20 @@ async def on_message(message):
     return
 
 
-  if message.content.startswith("$characters"):
-  
-    await message.channel.send(characters)
+  if message.content.startswith("$characters" ):
+     await message.channel.send(characters)
    
-  if message.content.startswith("$Glitch"):
+  if message.content.startswith("$glitch"):
     await message.channel.send(glitch)
     
-  
+  if message.content.startswith("$zawardo"):
+    await message.channel.send(zawardo)
+
+  if message.content.startswith("$timeleaper"):
+    await message.channel.send(timeleaper)
+
+  if message.content.startswith("$morph" ):
+    await message.channel.send(morph)
 
 client.run(os.getenv("work"))
 
