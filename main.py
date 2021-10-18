f798c99232bd137759d5a6b365441650090edc64
Angel Maria Stanley <88926570+Maiden23@users.noreply.github.com>
2021-10-18 02:25:33 +0000

    added few characters

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 
 import discord 
 import os
+
+
+
 client = discord.Client()
+
+
+characters = ["Glitch","Zawardo","timeleaper","morph"]
+glitch = "Thank you for choosing me , I have psychi powers and i hope i can help you:)"
 
 @client.event
 async def on_ready():
@@ -14,6 +21,17 @@ async def on_message(message):
 
   if message.content.startswith("$hello"):
     await message.channel.send("Hello")  
+    return
+
+
+  if message.content.startswith("$characters"):
+  
+    await message.channel.send(characters)
+   
+  if message.content.startswith("$Glitch"):
+    await message.channel.send(glitch)
+    
+  
 
 client.run(os.getenv("work"))
 
